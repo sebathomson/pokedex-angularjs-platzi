c0d1a29c8da4dcec9c9f888feda2c8021930e910
Sebastián Thomson <seba.thomson@gmail.com>
2015-10-11 23:54:53 -0300

    Upload project

---
+++ Notes.md
@@ -0,0 +1,14 @@
+## Notas que tomé en el curso
+
+Directivas: Porciones HTML para conectar DOM con Angular
+Plantillas/Expresiones: Funcionalidad
+Controladores: Lógica javascript
+Filtros: Hacer un procesado de info
+Formularios: Modelos de angular
+Módulos: Inyección de dependencias, arquitecturar y organizar
+Servicios: Crearlos o usarlos, la lógica específica de la app, conección con un backend de http
+CDN: Content Delivery Network
+
+- Para hacer funcionar AngularJs siempre hay que usar el ng-app
+
+- Para hacer válido el HTML es recomendable agregar el data- antes de las directivas.
---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+function getPokemons () {
+	return $.ajax({
+		url: 'assets/js/pokemons.json',
+		type: 'GET',
+		dataType: 'JSON',
+		async: false
+	})
+	.done(function(json) {
+		pokemons = json;
+	}).responseJSON;
+}
+
+function partition(data, n) {
+	return _.chain(data).groupBy(function (element, index) {
+		return Math.floor(index / n);
+	}).toArray().value();
+}
+
+
+(function () {
+
+	var app = angular.module('Pokedex', []);
+
+	app.controller('PokemonController', [ '$scope', function ($scope) {
+
+		$scope.pokemonGroups = partition( getPokemons(), 4 ) ;
+
+		console.log( $scope.pokemonGroups );
+
+	}]);
+
+})();
